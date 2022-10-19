6a8c9c75ccc709a8131f30fecea42a2019ece763
Nikhil Makhijani <72851103+nikhilmakhijani@users.noreply.github.com>
2022-10-19 12:35:14 +0530

    Update update_firestore.py

--- update_firestore.py
+++ update_firestore.py
@@ -4,7 +4,6 @@ data = {
     u'state': u'Delhi',
     u'country': u'India'
 }
-
 def addData(project_id,collection_name,document_id,data):
     db = firestore.Client(project=project_id)
     db.collection(collection_name).document(document_id).set(data)
@@ -13,8 +12,16 @@ def readData(project_id,collection_name):
     db = firestore.Client(project=project_id)
     users_ref = db.collection(collection_name)
     docs = users_ref.stream()
+    return docs
+
+def updateData(read_project_id, write_project_id, read_collection_name, write_collection_name, write_document_id):
+    docs = readData(read_project_id,read_collection_name)
     for doc in docs:
       print(f'{doc.id} => {doc.to_dict()}')
+      data = doc.to_dict()
+      result1 = pyjq.one('{Country: .country , Address: .name}', data )
+      addData(write_project_id,write_collection_name,write_document_id,result1)
+    
 
-addData("psychic-bliss-365216","cities","ndel",data)
-readData("psychic-bliss-365216","cities")
+      
+updateData("psychic-bliss-365216","flowing-sign-366006","cities","test","list")
